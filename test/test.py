169f139763e78f412f7f79c6f63ab86d0816e26b
Werner Robitza <slhck@users.noreply.github.com>
2021-08-17 14:06:20 +0200

    fix unit tests

--- test/test.py
+++ test/test.py
@@ -9,16 +9,25 @@ import G1072
 
 class TestLibrary:
     def test_library(self):
-        assert (
-            G1072.test_para(
-                bitrate=50,
-                framerate=60,
-                coding_res="1920x1080",
-                packetloss=0.2,
-                packetlossUDP=0,
-                delay=0,
-                Icomplexity="High",
-                Vcomplexity="High",
-            )
-            == 4.140085244657
+        MOS, all_results = G1072.test_para(
+            bitrate=50,
+            framerate=60,
+            coding_res="1920x1080",
+            packetloss=0.2,
+            packetlossUDP=0,
+            delay=0,
+            Icomplexity="High",
+            Vcomplexity="High",
         )
+
+        assert MOS == 4.580889112437288
+
+        assert all_results == {
+            "overall_quality": 4.580889112437288,
+            "interaction_quality_delay": 4.6229955301830845,
+            "interaction_quality_packetloss": 4.64,
+            "video_quality_based_on_refitted_g1071": 4.596755274928551,
+            "video_unclearness": 4.607621778600887,
+            "video_fragmentation": 4.471200161599724,
+        }
+
